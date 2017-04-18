c36a36bf7585bb0af9805a24a0aca57d84c20127
Luis del Cueto <luisdelcueto@live.com>
2017-04-18 04:06:06 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
-def my_select(collection)
- # your code here!
+def my_select(nums)
+  # your code here!
+  new_array = []
+  i = 0
+  while i < nums.length
+    value = yield(nums[i])
+    if value == true
+      new_array.push(nums[i])
+    end
+    i += 1
+  end
+  new_array
+end
+
+my_select([1, 2, 3, 4, 5]) do |num|
+  if num.even?
+    true
+  end
 end
